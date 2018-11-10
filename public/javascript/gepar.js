32f95672dafe98d673de51250c858b8a099604f0
Min-42 <mdecossas@ulmo.net>
2018-11-10 14:09:00 +0100

    gestion suppression en cours

--- public/javascript/gepar.js
+++ public/javascript/gepar.js
@@ -18,15 +18,16 @@ function gestDocuments() {
     $('#divDocuments').hide();
     listeDocs = initListeDocs();
 
-    var affichageDocs  = "";
+    var affichageDocs  = "<ul>";
     listeDocs.forEach(function(element, index, listeDocs) {
         obj = JSON.parse(element);
-        affichageDocs+='<img class="ico-suppress" title="supprimer" id="supprimerDocument"';
+        affichageDocs+='<li><img class="ico-suppress" title="supprimer" id="supprimerDocument"';
         affichageDocs+=" data-indice='"+index+"'";
         affichageDocs+=" data-type='"+obj.type+"'";
         affichageDocs+=' src="/images/ico-suppress.png">';
-        affichageDocs+='<a target="_blank" href="'+obj.fichier+'">'+obj.type+'</a><br>';
+        affichageDocs+='<a target="_blank" href="'+obj.fichier+'">'+obj.type+'</a></li>';
     });
+    affichageDocs+='</ul>';
     affichageDocs+='<a class="btn btn-sm btn-primary" href="#" id="ajouterDocument">Ajouter un document</a>';
 
     $('#divDocsListe').html(affichageDocs);
