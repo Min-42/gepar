b1570819a6145650849ccfa6c89ceb38567e3bc0
Min-42 <mdecossas@ulmo.net>
2018-11-10 14:15:01 +0100

    gestion suppression en cours 2

--- public/javascript/gepar.js
+++ public/javascript/gepar.js
@@ -1,36 +1,41 @@
-var listeDocs = new Array();
+$(function() {
+    var listeDocs = new Array();
+    gestDocuments();
 
-function initListeDocs() {
-    i=1;
-    tableau = [];
-    while (typeof($('#divDocuments option:eq('+i+')').html()) != "undefined") {
-        tableau.push($('#divDocuments option:eq('+i+')').html());
-        i++;
-    }
-    return tableau;
-}
-
-function supprimerDocument(indiceDoc, typeDoc) {
-alert("suppression");
-}
-
-function gestDocuments() {
-    $('#divDocuments').hide();
-    listeDocs = initListeDocs();
-
-    var affichageDocs  = "<ul>";
-    listeDocs.forEach(function(element, index, listeDocs) {
-        obj = JSON.parse(element);
-        affichageDocs+='<li><img class="ico-suppress" title="supprimer" id="supprimerDocument"';
-        affichageDocs+=" data-indice='"+index+"'";
-        affichageDocs+=" data-type='"+obj.type+"'";
-        affichageDocs+=' src="/images/ico-suppress.png">';
-        affichageDocs+='<a target="_blank" href="'+obj.fichier+'">'+obj.type+'</a></li>';
+    $('.ico-suppress').click(function(){
+        if (confirm('Voulez-vous supprimer le document "'+$(this).attr('data-type')+'"')){
+            $(this).parent().remove();
+        }
     });
-    affichageDocs+='</ul>';
-    affichageDocs+='<a class="btn btn-sm btn-primary" href="#" id="ajouterDocument">Ajouter un document</a>';
 
-    $('#divDocsListe').html(affichageDocs);
+    function initListeDocs() {
+        i=1;
+        tableau = [];
+        while (typeof($('#divDocuments option:eq('+i+')').html()) != "undefined") {
+            tableau.push($('#divDocuments option:eq('+i+')').html());
+            i++;
+        }
+        return tableau;
+    }
 
-    return true;
-}
+    function gestDocuments() {
+        $('#divDocuments').hide();
+        listeDocs = initListeDocs();
+
+        var affichageDocs  = "<ul>";
+        listeDocs.forEach(function(element, index, listeDocs) {
+            obj = JSON.parse(element);
+            affichageDocs+='<li><img class="ico-suppress" title="supprimer" id="supprimerDocument"';
+            affichageDocs+=" data-indice='"+index+"'";
+            affichageDocs+=" data-type='"+obj.type+"'";
+            affichageDocs+=' src="/images/ico-suppress.png">';
+            affichageDocs+='<a target="_blank" href="'+obj.fichier+'">'+obj.type+'</a></li>';
+        });
+        affichageDocs+='</ul>';
+        affichageDocs+='<a class="btn btn-sm btn-primary" href="#" id="ajouterDocument">Ajouter un document</a>';
+
+        $('#divDocsListe').html(affichageDocs);
+
+        return true;
+    }
+});
